4963dc3be84813d53b39db1d80d0d915b0bf7399
Abashin Yan <yan@nbmgroupe.com>
2015-10-30 17:33:28 +0100

    + headers

--- spyder.py
+++ spyder.py
@@ -3,16 +3,25 @@
 from bs4 import BeautifulSoup
 import urllib3
 
+#import redis
+
 '''
 Main fucntion
 
 '''
+
+request_headers = {
+'User-agent':'Mozilla/5.0 (compatible; prixanbot/0.1)'
+}
+
 def main(url):
     http = urllib3.PoolManager(timeout=5.0)
-    page = http.urlopen('GET', url)
+    page = http.urlopen('GET', url, headers=request_headers)
     soup = BeautifulSoup(page)
     soup.prettify()
     for link in soup.find_all('a'):
         print(link.get('href'))
 
-main('http://prixan.com/')
+main('http://www.antiagression.com')
+
+#r = redis.StrictRedis(host='localhost', port=6379, db=0)
