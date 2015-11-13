8e2bfc049ffa701cbcf0f5d0522c04e5718fbfca
koolkat <dev@koolkat.com>
2015-11-13 18:38:48 +0100

    fixed php script

--- php/get_info.php
+++ php/get_info.php
@@ -1,28 +1,37 @@
 <?php
+
 class RedisQueue {
     private $redis;
     function connect($addr='127.0.0.1', $port=6379) {
         $this->redis = new Redis();
         $this->redis->connect($addr, $port);
+        echo "connecting...\n";
     }
 
-    function rq_gen_pop($name=null) {
-        while (name and $this->redis->llen(name.':product_info')) {
-            yield $this->redis->lpop(name);
+    function rq_gen_pop($n) {
+        if (!$n) {
+            echo "No name specified to rq_get_data!";
+            return;
+        }
+        while ($n) {
+            #echo $this->redis->llen($n.':product_info');
+            #echo $n.':product_info'.'\n';
+            yield $this->redis->lpop($n);
         }
         return null;
     }
 
     function rq_get_data($name) {
-        $item_generator = rq_gen_pop(name.':product_info');
+        $item_generator = $this->rq_gen_pop($name.':product_info');
+        print_r($item_generator);
         foreach($item_generator as $item) {
-            print($item);
+            echo $item;
         }
     }
 
     function send_info($info, $name) {
         $info = json_encode($info);
-        $this->redis->rpush(name.':product_info', info);
+        $this->redis->rpush($name.':product_info', info);
     }
 
     function close() {
@@ -30,6 +39,7 @@ class RedisQueue {
     }
 }
 
-r = new RedisQueue();
-r->rq_get_data("http://le-narguile.com");
+$r = new RedisQueue();
+$r->connect();
+$r->rq_get_data("http://le-narguile.com");
 ?>
